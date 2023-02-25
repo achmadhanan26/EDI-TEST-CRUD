5933f8acb187ca80f2fb207fbd2c712c91ceddd2
achmad hanan darmawan <72918677+achmadhanan26@users.noreply.github.com>
2023-02-25 21:13:07 +0700

    Add files via upload

---
+++ GetDataUserInterface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserData
+{
+    [Serializable]
+    public class UserData
+    {
+        int intUserId = 0;
+        string szNamaLengkap = "";
+        string szUserName = "";
+        string szPassword = "";
+        bool bStatus = false;
+
+    }
+
+    public interface IEDITGetDataUser
+    {
+        UserData getDataUser(int intUserId);
+    }
+}
---
+++ GetUserDataManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserData
+{
+    public class GetUserData : IEDITGetDataUser
+    {
+        UserData IEDITGetDataUser.getDataUser(int intUserId)
+        {
+            throw null;
+        }
+    }
+}
